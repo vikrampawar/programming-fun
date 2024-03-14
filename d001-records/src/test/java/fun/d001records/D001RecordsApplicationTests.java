8c6c342748361ab11f134c4d976369078d549833
PawarV <pawar@rumq.net>
2024-03-14 22:27:41 +0000

    refact

--- d001-records/src/test/java/fun/d001records/D001RecordsApplicationTests.java
+++ d001-records/src/test/java/fun/d001records/D001RecordsApplicationTests.java
@@ -7,35 +7,59 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @SpringBootTest
-class D001RecordsApplicationTests {
+class D001RecordsApplicationTests
+{
 
-	@Test
-	void testInitial() {
-		// Traditional record created as a class
-		TraditionalRecord traditionalRecord = new TraditionalRecord(0,0);
-		assertThat(traditionalRecord).isNotEqualTo("TraditionalRecord{x=0, y=0}");
+    @Test
+    void testCreateARecord()
+    {
+        // Traditional record created as a class
+        TraditionalRecord traditionalRecord = new TraditionalRecord(0, 0);
+        assertThat(traditionalRecord).isNotEqualTo("TraditionalRecord{x=0, y=0}");
 
-		// Creating a record : Canonical Constructor
-		Record record = new Record(1, 1);
-		assertThat(record.toString()).isEqualTo("Record[x=1, y=1]");
+        // Creating a record : Canonical Constructor
+        Record record = new Record(1, 1);
+        assertThat(record.toString()).isEqualTo("Record[x=1, y=1]");
 
-		// immutable
-		traditionalRecord.x = 3;
-		traditionalRecord.setX(3);
-		assertThat(traditionalRecord.getX()).isEqualTo(3);
-		// record.x = 3;
-		// record.x() = 3;
+        // immutable
+        traditionalRecord.x = 3;
+        traditionalRecord.setX(3);
+        assertThat(traditionalRecord.getX()).isEqualTo(3);
+        // won't compile !!!
+        //!!! record.x = 3;
+        //!!! record.x() = 3;
+    }
 
-		// equals
-		var record2 = new Record(2,2);
-		assertThat(record2).isNotEqualTo(record);
+    @Test
+    void testEqualsHashCodeToString(){
+        Record record = new Record(1, 1);
 
-		// hashCode
-		assertThat(record.hashCode()).isEqualTo(32);
+        // equals
+        var record2 = new Record(2, 2);
+        assertThat(record2).isNotEqualTo(record);
 
-		// value object
-		var record2b = new Record(2, 2);
-		assertThat(record2).isEqualTo(record2b);
-	}
+        // hashCode
+        assertThat(record.hashCode()).isEqualTo(32);
+
+        // toString
+        assertThat(record.toString()).isEqualTo("Record[x=1, y=1]");
+    }
+
+    @Test
+    void testValueObject() {
+        var record2 = new Record(2, 2);
+        var record2b = new Record(2, 2);
+        assertThat(record2).isEqualTo(record2b);
+    }
+
+    @Test
+    void testAccessors() {
+        Record record = new Record(1, 1);
+        var x = record.x();
+        var y = record.y();
+        assertThat(x).isEqualTo(1);
+        assertThat(y).isEqualTo(1);
+
+    }
 
 }
