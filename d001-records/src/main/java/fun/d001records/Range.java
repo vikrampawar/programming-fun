af22f504a6463d8e0b6b090faee9e035eb48d59a
PawarV <pawar@rumq.net>
2024-03-15 07:44:21 +0000

    add range tests

--- d001-records/src/main/java/fun/d001records/Range.java
+++ d001-records/src/main/java/fun/d001records/Range.java
@@ -2,12 +2,19 @@ package fun.d001records;
 
 public record Range(int start, int end)
 {
-    public Range(int start, int end)
+    // block parameters are not declared
+    public Range
     {
+        // Can validate
         if (start > end) {
             throw new IllegalArgumentException("Start cannot be greater than end");
         }
-        this.start = start;
-        this.end = end;
+        // cant access the record's fields
+        // this.start = 13;
+        // But can change the parameters
+        if (start < 0)
+            start = 0;
+        if (end < 0)
+            end = 0;
     }
 }
--- d001-records/src/test/java/fun/d001records/D001RecordsApplicationTests.java
+++ d001-records/src/test/java/fun/d001records/D001RecordsApplicationTests.java
@@ -1,9 +1,13 @@
 package fun.d001records;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.assertj.core.api.AbstractThrowableAssert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 @SpringBootTest
@@ -11,27 +15,34 @@ class D001RecordsApplicationTests
 {
 
     @Test
-    void testCreateARecord()
+    void testTraditionalRecord()
     {
         // Traditional record created as a class
         TraditionalRecord traditionalRecord = new TraditionalRecord(0, 0);
         assertThat(traditionalRecord).isNotEqualTo("TraditionalRecord{x=0, y=0}");
+        // mutable
+        traditionalRecord.x = 3;
+        assertThat(traditionalRecord.x).isEqualTo(3);
+        traditionalRecord.setX(4);
+        assertThat(traditionalRecord.x).isEqualTo(4);
+    }
 
+    @Test
+    void testCreateARecord()
+    {
         // Creating a record : Canonical Constructor
         Record record = new Record(1, 1);
         assertThat(record.toString()).isEqualTo("Record[x=1, y=1]");
 
         // immutable
-        traditionalRecord.x = 3;
-        traditionalRecord.setX(3);
-        assertThat(traditionalRecord.getX()).isEqualTo(3);
         // won't compile !!!
         //!!! record.x = 3;
         //!!! record.x() = 3;
     }
 
     @Test
-    void testEqualsHashCodeToString(){
+    void testEqualsHashCodeToString()
+    {
         Record record = new Record(1, 1);
 
         // equals
@@ -46,20 +57,42 @@ class D001RecordsApplicationTests
     }
 
     @Test
-    void testValueObject() {
+    void testValueObject()
+    {
         var record2 = new Record(2, 2);
         var record2b = new Record(2, 2);
         assertThat(record2).isEqualTo(record2b);
     }
 
     @Test
-    void testAccessors() {
+    void testAccessors()
+    {
         Record record = new Record(1, 1);
         var x = record.x();
         var y = record.y();
         assertThat(x).isEqualTo(1);
         assertThat(y).isEqualTo(1);
-
     }
 
+    @Test
+    void testCompactConstructor()
+    {
+        Range range = new Range(1, 5);
+        System.out.println(range);
+    }
+
+    @Test
+    void testStartGreaterThanEnd()
+    {
+        var exception = assertThrows(IllegalArgumentException.class, () -> new Range(5, 1));
+        assertThat(exception.getMessage()).isEqualTo("Start cannot be greater than end");
+    }
+
+    @Test
+    void testReassignmentOfParameters()
+    {
+        var range = new Range(-10, -3);
+        assertThat(range.start()).isEqualTo(0);
+        assertThat(range.end()).isEqualTo(0);
+    }
 }
